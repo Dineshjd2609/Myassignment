3e67599c85869b0e47f0b16968843083b9ce5906
Dineshjd2609 <dineshjd7@gmail.com>
2025-12-17 12:42:23 +0530

    DAY1 assignment

---
+++ primitive.js
@@ -0,0 +1,10 @@
+var Name="Dinesh"
+var organization="testleaf"
+var contact=9597976550
+var isAutomation=true
+var play
+console.log("Testername" + ":"+ Name + typeof Name)
+console.log("OrganizationName"+":" + organization + typeof organization)
+console.log("contactNumber" +":" +contact + typeof contact)
+console.log("HasplaywrightExperience" + ":" +play + typeof play)
+console.log("isAutomationTESTER"+" :"+isAutomation + typeof isAutomation)

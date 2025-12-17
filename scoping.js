8d85c60809fab5bbe2f694bd270ab1c1eaee49f1
Dineshjd2609 <dineshjd7@gmail.com>
2025-12-17 12:45:12 +0530

    scoping -DAY2 Assignment

---
+++ scoping.js
@@ -0,0 +1,12 @@
+var browserversion=10.2
+{
+    var browserName="opera"
+    browserName="chrome"
+    let browserversion=10.12
+    const loadurl="https://web.whatsapp.com/"
+    console.log("Inside block:"+browserName)
+    console.log("Inside block:"+browserversion)
+    console.log("insideblock"+ loadurl)
+
+}
+console.log("outsideblock:"+browserversion)

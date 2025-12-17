8b17b351af33456883d6bff7b1c2b5c1218ccf4c
Dineshjd2609 <dineshjd7@gmail.com>
2025-12-17 12:48:33 +0530

    If condition Assignment-Day2

---
+++ condition.js
@@ -0,0 +1,15 @@
+let mark=69
+if(mark>=90){
+    console.log("A grade")
+}
+else if (mark>=80 && 80>=mark<=89)
+{
+    console.log("bgrade")
+}
+else if(mark>=70 && 70>=mark<79)
+{
+    console.log("c grade")
+}
+else{
+  console.log("fail")
+}

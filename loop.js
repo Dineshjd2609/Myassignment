bb345e51d05ad6167530e5459bc25336d2a2b55e
Dineshjd2609 <dineshjd7@gmail.com>
2025-12-18 18:09:29 +0530

    Day-3 class room task

---
+++ loop.js
@@ -0,0 +1,6 @@
+let num=20
+for(let i=num; i>0;i--){
+    if(i%2 !==0){
+        console.log("ODD number is" + i)
+    }
+}
---
+++ switchcase.js
@@ -0,0 +1,21 @@
+let day=6
+switch(day){
+    case 1:
+        console.log("DAY is sunday")
+        break
+    case 2:
+        console.log("Day is monday")
+        break
+    case 3:
+        console.log("Day is tuesday")
+        break
+    case 4:
+        console.log("Day is wednesday")
+        break
+    case 5:
+        console.log("Day is thursday")
+        break
+    default:
+        console.log("Day is not specified")
+       
+}
